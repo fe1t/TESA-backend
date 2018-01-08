6df035ec2be64b07d6adfc4154f9ae528fa028c2
Nuttapon Thanitsukkan <nuttapon.t@ku.th>
2018-01-08 23:53:32 +0700

    add nodemon for monitoring changes :wrench:

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 import { Animal } from 'src/animal/model'
 import assert from 'assert'
+import bodyParser from 'body-parser'
 import dbConfig from './config/db'
 import express from 'express'
 import mongoose from 'mongoose'
 
 const app = express()
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
 
 mongoose.connect(dbConfig.url + dbConfig.name, {})
 mongoose.Promise = global.Promise
@@ -15,11 +18,6 @@ const cats = new Animal({
 
 cats.save(err => {
   assert.equal(null, err)
-  Animal.find({}, (err, res) => {
-    assert.equal(null, err)
-    // eslint-disable-next-line no-console
-    console.log(res)
-  })
 })
 
 var db = mongoose.connection
@@ -29,5 +27,20 @@ db.on('error', console.error.bind(console, 'MongoDB connection error:'))
 app.get('/', (req, res) => {
   return res.send('Hello World')
 })
+
+app.get('/temps', (req, res) => {
+  const output = {
+    fuck: [1, 2, 3, 4, 5]
+  }
+  return res.end(JSON.stringify(output))
+})
+
+app.get('/humandities', (req, res) => {
+  const output = {
+    humandities: [10, 20, 30, 40]
+  }
+  return res.end(JSON.stringify(output))
+})
+
 // eslint-disable-next-line no-console
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
